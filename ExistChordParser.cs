19b870b38b89ab3274ba7d843f0327142a774422
AlexeyGuryev <alguryew@gmail.com>
2016-08-13 00:55:22 +0300

    try to fix linux path directory slash symbol separator

--- ExistChordParser.cs
+++ ExistChordParser.cs
@@ -15,7 +15,7 @@ namespace TelegramBot
 
         public string FindExistChord(string sequence)
         {
-            var fileName = string.Format("{0}\\{1}.mp3", _path, sequence.ToUpper());
+            var fileName = string.Format("{0}{1}{2}.mp3", _path, Path.DirectorySeparatorChar, sequence.ToUpper());
             var files = Directory.EnumerateFiles(_path);
             return files.FirstOrDefault(f => string.Equals(f, fileName, StringComparison.OrdinalIgnoreCase));
         }
